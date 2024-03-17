ec1c32550dd0aa204607921df84d0591a75b8e60
ManhHoangLab <manhhoang.jw@gmail.com>
2024-03-17 23:31:27 +0700

    add database

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 const app = express();
 const port = 3000;
+const { Client } = require('pg');
+
+//database
+const client = new Client({
+    user: "postgres",
+    password: "123456",
+    host: "localhost",
+    port: 5432,
+    database: "demodb"
+
+})
+
+client.connect()
+    .then(() => console.log("Connected successfuly"))
+    .then(() => client.query("select * from users"))
+    .then(results => console.table(results.rows))
+    .catch(e => console.log)
+    .finally(() => client.end())
+
 
 const route = require('./routes');
 
